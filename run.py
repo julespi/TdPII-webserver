a118be4f85e4081599c035aa0c4a9104caca174c
julespi@gmail.com <julespi@gmail.com>
2019-09-29 19:18:10 -0300

    url con metodo get funcionando para temp y vol

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 #from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index')
 def index():
-    return 'Pagina de Indexxx'
+    return 'Pagina de Index'
+
+@app.route('/poster',methods=["GET"])
+def poster():
+    if request.method=="GET":
+        try:
+            temp=request.args.get("temp")
+            vol=request.args.get("vol")
+            respuesta='Temperatura={}<br/>Volumen={}'.format(str(float(temp)),str(float(vol)))
+            return respuesta
+        except:
+            return "get simple"
+    else:
+        return "falla"
