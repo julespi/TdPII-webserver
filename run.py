8ce82a24a9069df4012255c42eb92ee17d886fb7
julespi@gmail.com <julespi@gmail.com>
2019-10-03 20:47:07 -0300

    no creo q haya nada nuevo

--- run.py
+++ run.py
@@ -1,10 +1,16 @@
 from flask import Flask, request
+#import logging
+
+
 #from flask import render_template
 app = Flask(__name__)
+
+
 
 @app.route('/')
 @app.route('/index')
 def index():
+    #app.logger.info('Info')
     return 'Pagina de Index'
 
 @app.route('/poster',methods=["GET"])
@@ -13,9 +19,19 @@ def poster():
         try:
             temp=request.args.get("temp")
             vol=request.args.get("vol")
-            respuesta='Temperatura={}<br/>Volumen={}'.format(str(float(temp)),str(float(vol)))
+            id=request.args.get("id")
+            time=request.args.get("time")
+            respuesta='Id={}<br/>Time={}<br/>Temperatura={}<br/>Volumen={}'.format(str(id),str(time),str(float(temp)),str(float(vol)))
             return respuesta
         except:
             return "get simple"
     else:
         return "falla"
+
+# if __name__=="__main__":
+#     app.run(host="0.0.0.0",debug="True")
+#     import logging
+#     app.logger.INFO('asd')
+#     logging.basicConfig(filename='errossr.log',level=logging.INFO)
+
+    
