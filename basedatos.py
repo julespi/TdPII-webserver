1eae690b0ba14adc26e4fe146947efb6c8bebb6e
julespi@gmail.com <julespi@gmail.com>
2019-10-21 18:28:35 -0300

    C:/Program Files/Git/version funcionando

--- basedatos.py
+++ basedatos.py
@@ -31,13 +31,15 @@ class Nodo(db.Model):
     nodo = db.Column("nodo", db.INTEGER)
     cliente = db.Column("id_cliente", db.INTEGER)
     firmware = db.Column("firmware", String(10))
+    timestamp_fm = db.Column("timestamp_fm", db.DATETIME)
     nombre = db.Column("nombre", String(50))
     descripcion = db.Column("descripcion", String(100))
 
 
-    def __init__(self, nodo, cliente, firmware, nombre, descripcion):
+    def __init__(self, nodo, cliente, firmware, timestamp_fm, nombre, descripcion):
         self.nodo = nodo
         self.cliente = cliente
         self.firmware = firmware
+        self.timestamp_fm = timestamp_fm
         self.nombre = nombre
         self.descripcion = descripcion
